15a56aa65c61744a5dd37d1a6dac1ade0fd87956
angelina-tsuboi <angelina.t1832@gmail.com>
2023-07-26 17:13:04 -0700

    feat: improve banner

--- modules/banner.py
+++ modules/banner.py
@@ -1,9 +1,17 @@
+import os
+from rich.console import Console
+
+console = Console()
 
 def banner():
-    f = open('/assets/banner.txt', 'r')
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    file_name = os.path.join(file_dir, './assets/banner.txt')
+    f = open(file_name, 'r')
     file_contents = f.read()
-    print(file_contents)
+    console.print("\n [cyan]" + file_contents + "[/]")
     f.close()
+    console.print("[blue bold]    Created by Angelina Tsuboi (angelinatsuboi.com)[/]")
+    console.print("              [blue underline]github.com/ANG13T/ClimaCast[/] \n")
 
 def menu_print():
-    print("===== ClimaCast Options =====")
+    print("==================== ClimaCast Options ====================")
